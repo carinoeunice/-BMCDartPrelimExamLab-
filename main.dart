0f9e7469886ed1c6e240211c1d49bf374f90be41
carinoeunice <ecarino021920@gmail.com>
2025-08-28 09:55:26 +0000

    Student Grade Evaluator_Cariño

--- main.dart
+++ main.dart
@@ -1,32 +1,32 @@
 import 'dart:io';
 
 void main() {
-
+//Declare constants
   const.double passingGradeThreshold = 60.0; 
   const.double maxScore = 100.0;             
   const.double assignmentWeight = 0.2;       
   const.double midtermWeight = 0.3;          
   const.double finalExamWeight = 0.5;        
 
- 
+ //Ask for student's name
   stdout.write("Enter student's name: ");
   String studentName = stdin.readLineSync() ?? "Unknown";
 
-  
+//Ask for scores with validation  
   double assignmentScore = getValidScore("Assignment", maxScore);
   double midtermScore = getValidScore("Midterm", maxScore);
   double finalExamScore = getValidScore("Final Exam", maxScore);
 
   
-  
+//Compute final grade 
   double finalGrade = (assignmentScore * assignmentWeight) +
       (midtermScore * midtermWeight) +
       (finalExamScore * finalExamWeight);
 
-  
+//Determine Pass/Fail  
   String status = finalGrade >= passingGradeThreshold ? "Passed " : "Failed ";
 
-  
+//Display results  
   print("\n===== STUDENT GRADE REPORT =====");
   print("Student Name   : $studentName");
   print("Assignment     : ${assignmentScore.toStringAsFixed(1)}");
